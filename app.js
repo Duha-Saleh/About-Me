c6d62cf4b6ae136f8c23ff59ec28ea5962751c82
Duha-Saleh <duhamahmoud096@gmail.com>
2020-06-10 13:30:25 +0300

    last changge

--- app.js
+++ app.js
@@ -23,63 +23,64 @@ if (know == 'y' || know == 'yes') {
     grade++;
 }
 
-    //console.log(know)
-    alert("My Major is physics , and its scientific as you see ")
-    var programming = prompt("Do I love programming ?").toLowerCase();
-    //console.log(programming)
-    while (programming != 'y' && programming != 'yes' && programming != 'no' && programming != 'n') {
-        programming = prompt("Do I love programming ?", 'y').toLowerCase();
-    }
-    alert("I do love programming")
-    if (programming == 'y' || programming == 'yes') {
+//console.log(know)
+alert("My Major is physics , and its scientific as you see ")
+var programming = prompt("Do I love programming ?").toLowerCase();
+//console.log(programming)
+while (programming != 'y' && programming != 'yes' && programming != 'no' && programming != 'n') {
+    programming = prompt("Do I love programming ?", 'y').toLowerCase();
+}
+alert("I do love programming")
+if (programming == 'y' || programming == 'yes') {
+
+    grade++;
+}
+
+var physics = prompt("do I hate my major(physics) ?").toLowerCase();
+//console.log(physics)
+while (physics != 'y' && physics != 'yes' && physics != 'no' && physics != 'n') {
+    physics = prompt("do I hate my major(physics) ?", 'y').toLowerCase();
+}
+alert("absolutly I love physics! ")
+if (physics == 'y' || physics == 'yes') {
+
+    grade++;
+}
+
+var course = prompt("is the programming course useful for my future dream and career ?").toLowerCase();
+//console.log(course)
+while (course != 'y' && course != 'yes' && course != 'no' && course != 'n') {
+    course = prompt("is the programming course useful for my future dream and career ?", 'y').toLowerCase();
+
+    alert("yes , so much , it will help me achieve my dream! ")
+
+    if (course == 'y' || course == 'yes') {
 
         grade++;
+
     }
 
-        var physics = prompt("do I hate my major(physics) ?").toLowerCase();
-        //console.log(physics)
-        while (physics != 'y' && physics != 'yes' && physics != 'no' && physics != 'n') {
-            physics = prompt("do I hate my major(physics) ?", 'y').toLowerCase();
-        }
-        alert("absolutly I love physics! ")
-        if (physics =='y' || physics =='yes'){ 
-    
+    for (var x = 0; x < 4; x++) {
+        var userinput = prompt("how many top countries do I like to visit?", 'type a number');
+
+        if (userinput == 10) {
             grade++;
+
+            break;
         }
-            
-        var course = prompt("is the programming course useful for my future dream and career ?").toLowerCase();
-        //console.log(course)
-        while (course != 'y' && course != 'yes' && course != 'no' && course != 'n') {
-            course = prompt("is the programming course useful for my future dream and career ?", 'y').toLowerCase();
+        else if (userinput > 10) { alert("too high!"); }
+        else if (userinput < 10) { alert("too low!"); }
+    }
+    alert(" the correct answer is 10 ")
 
-        alert("yes , so much , it will help me achieve my dream! ")
-
-        if (course =='y' || course =='yes'){ 
-    
+    var arraycolor = ["red", "blue", "white", "pink"]
+    for (var m = 0; m < 6; m++) {
+        var colors = prompt("Suggest a name of a color I love?").toLowerCase();
+        if (colors == arraycolor[0] || colors == arraycolor[1] || colors == arraycolor[2] || colors == arraycolor[3]) {
             grade++;
-            
-        }
-
-        for (var x = 0; x < 4; x++) {
-            var userinput = prompt("how many top countries do I like to visit?", 'type a number');
-
-            if (userinput == 10) {
-               
-                break;
-            }
-            else if (userinput > 10) { alert("too high!"); }
-            else if (userinput < 10) { alert("too low!"); }
-        }
-        alert(" the correct answer is 10 ")
-
-        var arraycolor = ["red", "blue", "white", "pink"]
-        for (var m = 0; m < 6; m++) {
-            var colors = prompt("Suggest a name of a color I love?").toLowerCase();
-            if (colors == arraycolor[0] || colors == arraycolor[1] || colors == arraycolor[2] || colors == arraycolor[3]) {
-                grade++;
-                break;
-            }
+            break;
         }
     }
-    alert("the right answers are : blue white pink and red")
-    alert(" your grade is " +grade +"out of 7")
+}
+alert("the right answers are : blue white pink and red")
+alert(" your grade is " + grade + "out of 7")
